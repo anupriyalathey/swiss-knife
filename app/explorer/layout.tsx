470c0137375e393db73da0d8a377cba0de3edae5
anupriyalathey <anupriyalathey@gmail.com>
2024-02-15 17:38:03 +0530

    added search bar

--- app/explorer/layout.tsx
+++ app/explorer/layout.tsx
@@ -15,12 +15,14 @@ import {
   HStack,
   Spacer,
 } from "@chakra-ui/react";
-import { SearchIcon } from "@chakra-ui/icons";
+import { SearchIcon } from "@chakra-ui/icons";  
 import { isAddress } from "viem";
 import { normalize } from "viem/ens";
 import { publicClient, getPath } from "@/utils";
 import subdomains from "@/subdomains";
 import { Layout } from "@/components/Layout";
+import { ExplorersData } from "@/types";
+// import { Explorer } from "./page";
 
 const isValidTransaction = (tx: string) => {
   return /^0x([A-Fa-f0-9]{64})$/.test(tx);
@@ -35,9 +37,16 @@ const ExplorerLayout = ({ children }: { children: ReactNode }) => {
   const userInputFromUrl = segments[1] ?? segments[0];
 
   const [userInput, setUserInput] = useState<string>(userInputFromUrl);
+  const [query, setQuery] = useState<string>();
   const [isInputInvalid, setIsInputInvalid] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  
+  // const [filteredExplorers, setFilteredExplorers] = useState<ExplorersData[]>();
 
+  // const [safeDapps, setSafeDapps] = useState<{
+  //   [networkId: number]: SafeDappInfo[];
+  // }>({});
+  
   const handleSearch = async (_userInput?: string) => {
     setIsLoading(true);
 
@@ -108,6 +117,11 @@ const ExplorerLayout = ({ children }: { children: ReactNode }) => {
     }
   }, [isInputInvalid]);
 
+  interface QuerySearchParams {
+    query: string | undefined;
+    setQuery: (value: string) => void;
+  }
+
   return (
     <Layout>
       <Center flexDir={"column"} mt="5">
@@ -155,7 +169,39 @@ const ExplorerLayout = ({ children }: { children: ReactNode }) => {
             </Button>
           </InputRightElement>
         </InputGroup>
-        <Box mt="5">{children}</Box>
+
+        {/* -------------- search for explorers ------------------- */}
+        <HStack mt="1rem" w="60%">
+          <Heading fontSize={"xl"}>Search Explorers</Heading>{" "}
+          <Spacer />
+        </HStack>
+        <InputGroup mt="1rem" maxW="60%">
+          <Input
+            placeholder="address explorers"
+            value={query}
+            onChange={(e) => {
+              setQuery(e.target.value);
+            }}
+          /> 
+          
+           {query && (
+          <InputRightElement w="4rem">
+            <Button
+              mr="0.5rem"
+              w="100%"
+              size="sm"
+              colorScheme={isInputInvalid ? "red" : "blue"}
+              onClick={() => setQuery("")}
+              isLoading={isLoading}
+            >
+              <SearchIcon />
+            </Button>
+          </InputRightElement>
+            )} 
+        </InputGroup>
+        <Box mt="5">
+                {children}
+        </Box>
       </Center>
     </Layout>
   );
